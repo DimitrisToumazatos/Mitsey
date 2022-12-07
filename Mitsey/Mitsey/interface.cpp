2ac927130e345ea42b1df305575a3e2aebde0df4
Dimitris Toumazatos <di.toumazatos@gmail.com>
2022-12-07 21:04:58 +0200

    add bar

--- Mitsey/Mitsey/interface.cpp
+++ Mitsey/Mitsey/interface.cpp
@@ -4,14 +4,34 @@
 
 void Interface::init()
 {
+	graphics::playMusic(std::string(ASSET_PATH) + "background-music.mp3", 0.2f, true, 2000);   // background music 
 }
 
 void Interface::draw()
 {
 	graphics::Brush br;
-	br.texture = std::string(ASSET_PATH) + "cyan1.png";
+
+	br.fill_color[0] = 0.2f;
+	br.fill_color[1] = 0.2f;
+	br.fill_color[2] = 0.2f;
+
 	br.outline_opacity = 0.f;
 	graphics::drawRect(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, CANVAS_WIDTH, CANVAS_HEIGHT, br);
+
+	br.texture = std::string(ASSET_PATH) + "bar-shadow1.png";
+	graphics::drawRect(CANVAS_WIDTH / 2, 82, CANVAS_WIDTH+300, 40, br);
+
+	br.texture = "";
+
+	br.fill_color[0] = 0.0f;
+	br.fill_color[1] = 0.0f;
+	br.fill_color[2] = 0.2f;
+
+	br.outline_opacity = 0.0;
+
+	br.fill_opacity = 0.5f;
+
+	graphics::drawRect(CANVAS_WIDTH/2, 40, CANVAS_WIDTH, 80, br);
 }
 
 void Interface::update()
